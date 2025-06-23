04d0e333c1d7d270e50c2d26b5491bb230cda774
Nkululeko S. Zwane <124434983+nkululeko111@users.noreply.github.com>
2025-06-23 14:11:33 +0200

    Update socket.js

--- socket.js
+++ socket.js
@@ -1,4 +1,4 @@
-import { io } from "socket.io-client";
+const { io } = require("socket.io-client");
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
 let socketInstance = null;
